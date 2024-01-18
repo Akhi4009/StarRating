8c3cd5518f46bbe079ed2cba4fd37eb99f127f9d
Akhilesh yadav <akhileshyadav310596@gmail.com>
2024-01-18 20:43:58 +0530

    starRating component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import StarRating from "./StarRating"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <StarRating maxRating={5} message={['Terrible','Bad','Okay','Good','Amazing']}/>
+    <StarRating maxRating={5} color='#f91111' size={20}/>
+    </>
   );
 }
 
